e6691b2a6c62bd36924b029953121f26cfbb6a28
rgurgul <gurgul.robert@gmail.com>
2026-04-15 08:13:48 +0200

    ref

--- js/index.js
+++ js/index.js
@@ -6,17 +6,18 @@ import { display } from "./results.js";
 
 let pacmanRef = document.body.querySelector(".hero");
 let imgs = ["./images/gift.png", "./images/gift2.png", "./images/gift3.png", "./images/skucha.png"];
-document.addEventListener("result", display);
 let gift = new Gift(imgs, pacmanRef);
-document.body.addEventListener("keydown", ({ code }) => {
-  ["ArrowLeft", "ArrowRight"].some((v) => v === code) && startMoving(code);
-  code === "Space" && new Arrow("./images/arrow.png", pacmanRef.getBoundingClientRect(), gift.img);
-});
-document.body.addEventListener(
-  "keyup",
-  ({ code }) => ["ArrowLeft", "ArrowRight"].some((v) => v === code) && stopMoving(),
-);
-document.addEventListener("move", bgMove);
 moveSettings.target = pacmanRef;
 bgSettings(["./images/bg2.png", "./images/bg1.png"]);
 setInterval(() => display({ detail: { time: 1 } }), 1000);
+const go = ({ code }) => {
+  ["ArrowLeft", "ArrowRight"].some((v) => v === code) && startMoving(code);
+  code === "Space" && new Arrow("./images/arrow.png", pacmanRef.getBoundingClientRect(), gift.img);
+};
+const stop = ({ code }) => ["ArrowLeft", "ArrowRight"].some((v) => v === code) && stopMoving();
+
+document.body.addEventListener("keydown", go);
+document.body.addEventListener("keyup", stop);
+document.addEventListener("move", bgMove);
+document.addEventListener("result", display);
+
